e86205693813a8cfc1b0d2643cb04fde58bfe149
Sergey <serg.v.sergienko@gmail.com>
2025-02-21 18:01:14 +0100

    Make InventoriesPage prerendered

--- src/app/(domain)/inventories/page.tsx
+++ src/app/(domain)/inventories/page.tsx
@@ -2,8 +2,6 @@ import { DataTable } from '@/components/ui/data-table';
 import { columns } from './columns';
 import { getInventories } from '@/actions/inventory-actions';
 
-export const dynamic = 'force-dynamic'; // TODO: implement revalidatePath('/tanks')
-
 export default async function InventoriesPage() {
   const inventories = await getInventories();
 
